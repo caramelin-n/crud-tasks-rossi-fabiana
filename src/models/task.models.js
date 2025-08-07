da29d9e84c42e30baea27da52c8d98f63a576935
Rossi F. <Rossifabiana930@gmail.com>
2025-08-07 10:25:45 -0300

    Modelos terminados.

--- src/models/task.models.js
+++ src/models/task.models.js
@@ -1,1 +1,10 @@
-. 
+import sequelize from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+const task_models = sequelize.define("task", {
+    title: { type: DataTypes.STRING, unique: true, allowNull:false },
+    description: { type: DataTypes.STRING, allowNull: false },
+    isComplete: { type: DataTypes.BOOLEAN, defaultValue: false }
+});
+
+export default task_models;
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,1 +1,10 @@
-. 
+import sequelize from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+const user_models = sequelize.define("user", {
+    name: { type: DataTypes.STRING, allowNull:false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
+});
+
+export default user_models;
