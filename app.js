df2318359c1f59b246b683d83d6a2e2b57f1589f
Rossi F. <Rossifabiana930@gmail.com>
2025-08-07 10:12:11 -0300

    db y database hechos.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-. 
+import express from express;
+import dotenv from "./src/config/database.js";
+import sequelize from "./src/config/database.js";
+import db from "./src/config/db.js";
+import task_routes from "./src/routes/task.routes.js";
+import user_routes from "./src/routes/user.routes.js";
--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,15 @@
-. 
+import sequelize from "./database.js";
+import chalk from "chalk";
+
+const db = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(chalk.greenBright("La conexión a la base de datos ha sido exitosa."))
+        await sequelize.sync();
+    } catch (error) {
+        console.error(chalk.redBright("No se pudo conectar a la base de datos."));
+        console.error(chalk.blue(error));
+    };
+};
+
+export default db;
